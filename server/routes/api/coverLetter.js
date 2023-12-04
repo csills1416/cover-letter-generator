39d704dd5a1a55abbd907af7a20354c14eb17fa3
CSillus <71670415+csills1416@users.noreply.github.com>
2023-12-04 13:02:16 -0500

    added controller

--- server/routes/api/coverLetter.js
+++ server/routes/api/coverLetter.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { generateCoverLetter } = require('../../controllers/coverLetterController');
 
 module.exports = function(openai) {
-    // POST route to generate a cover letter
     router.post('/generate', async (req, res) => {
         const { name, jobTitle, companyName, additionalInfo } = req.body;
 
         try {
-            // Use OpenAI API to generate the cover letter
-            const response = await openai.createCompletion({
-                model: "text-davinci-003", // Or the latest available model
-                prompt: `Write a professional cover letter for a person named ${name}, applying for the position of ${jobTitle} at ${companyName}. Additional information: ${additionalInfo}`,
-                max_tokens: 500
-            });
-
-            const coverLetter = response.data.choices[0].text.trim();
+            const coverLetter = await generateCoverLetter(openai, name, jobTitle, companyName, additionalInfo);
             res.json({ coverLetter });
         } catch (error) {
-            console.error('OpenAI Error:', error);
             res.status(500).send('Error generating cover letter');
         }
     });
