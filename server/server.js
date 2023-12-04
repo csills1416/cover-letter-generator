61a927cf6bfc5d002aa21ef2c0ea4f7c24f3373c
CSillus <71670415+csills1416@users.noreply.github.com>
2023-12-04 12:28:06 -0500

    removed access file, added OPENAI key to server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const { Configuration, OpenAIApi } = require("openai");
+
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
-require('dotenv').config();
+
+// OpenAI configuration
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 app.use(express.json());
 
@@ -10,10 +19,24 @@ app.get('/', (req, res) => {
 });
 
 // POST route to generate a cover letter
-app.post('/generate-cover-letter', (req, res) => {
-    // Extracting data from the request body
-    const { name, jobTitle, companyName, additionalInfo } = req.body;
-});  
+app.post('/generate-cover-letter', async (req, res) => {
+  const { name, jobTitle, companyName, additionalInfo } = req.body;
+
+  try {
+    // Use OpenAI API to generate the cover letter
+    const response = await openai.createCompletion({
+      model: "text-davinci-003", // Or the latest available model
+      prompt: `Write a professional cover letter for a person named ${name}, applying for the position of ${jobTitle} at ${companyName}. Additional information: ${additionalInfo}`,
+      max_tokens: 500
+    });
+
+    const coverLetter = response.data.choices[0].text.trim();
+    res.json({ coverLetter });
+  } catch (error) {
+    console.error('OpenAI Error:', error);
+    res.status(500).send('Error generating cover letter');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
