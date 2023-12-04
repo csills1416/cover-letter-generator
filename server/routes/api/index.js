010e2238013f96ab3d9a8645878a73d371a62364
CSillus <71670415+csills1416@users.noreply.github.com>
2023-12-04 14:36:40 -0500

    changed into React.js

--- server/routes/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const coverLetterRoute = require('./api/coverLetter');
-
-router.use('/cover-letter', coverLetterRoute(openai));
-
-module.exports = router;
